8dfbffbd1acbfd76507da20aed7e1af830fbe037
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-12 10:00:37 -0400

    Added and Defined Variables

--- src/MathTester.java
+++ src/MathTester.java
@@ -1,6 +1,10 @@
 public class MathTester {
     public static void main(String[] args){
-       
+       String FirstWord;
+       String SecondWord;
+
+       FirstWord = "Dog";
+       SecondWord = "house";
 
 
 
