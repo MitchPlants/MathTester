02e11fc3f29bb853ecae4e7a7eaf8df4b64fe8db
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-12 10:05:38 -0400

    added and defined number variables

--- src/MathTester.java
+++ src/MathTester.java
@@ -2,9 +2,13 @@ public class MathTester {
     public static void main(String[] args){
        String FirstWord;
        String SecondWord;
+       int FirstNumber,SecondNumber;
+
 
        FirstWord = "Dog";
        SecondWord = "house";
+       FirstNumber = 5;
+       SecondNumber = 10;
 
        System.out.println(FirstWord+SecondWord);
 
