311f5a9764f9b101ee08abc3ef9b090391b096fa
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-13 09:27:02 -0400

    Changed Math Operations to a block comment and added Totals and Total+10

--- src/MathTester.java
+++ src/MathTester.java
@@ -2,21 +2,27 @@ public class MathTester {
     public static void main(String[] args){
        String FirstWord;
        String SecondWord;
-       int FirstNumber,SecondNumber;
+       int FirstNumber,SecondNumber,Total;
 
 
        FirstWord = "Dog";
        SecondWord = "house";
        FirstNumber = 5;
        SecondNumber = 10;
-       System.out.println(FirstWord+SecondWord);
+       Total = FirstNumber+SecondNumber;
+       System.out.println(Total);
+       Total = Total+10;
+       System.out.println(Total);
+
+
+       /*System.out.println(FirstWord+SecondWord);
        System.out.println(FirstWord+(FirstNumber+SecondNumber));
        System.out.println("FirstNumber + SecondNumber is "+(FirstNumber+SecondNumber));
        System.out.println("FirstNumber - SecondNumber is "+ (FirstNumber-SecondNumber));
        System.out.println("SecondNumber / FirstNumber is "+ (SecondNumber/FirstNumber));
        System.out.println("FirstNumber * SecondNumber is "+ (FirstNumber*SecondNumber));
        System.out.println("The modulus of the secondNumber by the firstNumber" +
-       " is "+(SecondNumber%FirstNumber));
+       " is "+(SecondNumber%FirstNumber));*/
 
 
 
